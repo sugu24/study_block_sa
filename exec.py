6ec2a4d0571daea3b24c5fe91d0b5c551fb1b6f7
sugu24 <shayasugu@icloud.com>
2024-01-15 12:27:11 +0900

    c

--- exec.py
+++ exec.py
@@ -7,11 +7,11 @@ max_N = 5000
 
 # start thread
 exe = []
-for w in range(2, 11):
-    exe.append(("./sudoku_one.exe", w, w))
-    exe.append(("./sudoku.exe", w, w))
-    exe.append(("./sudoku_one.exe", w, 2))
-    exe.append(("./sudoku.exe", w, 2))
+for w in range(2, 13):
+    for rho in range(2, w+1):
+        exe.append(("./sudoku.exe", w, rho))
+        exe.append(("./sudoku_one.exe", w, rho))
+
 # exe = [(4, 2),(4, 2),(3, 2), (3, 3), (4, 2), (4, 3), (4, 4), (5, 2), (5, 3), (5, 4), (5, 5), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6), (7, 2), (7, 3), (7, 4), (7, 5), (7, 6), (7, 7), (8, 2), (8, 3), (8, 4), (8, 5), (8, 6), (8, 7), (8, 8)]
 # exe = [(9, 2), (9, 3), (9, 4), (9, 5), (9, 6), (9, 7), (9, 8), (9, 9), (10, 2), (10, 3), (10, 4), (10, 5), (10, 6), (10, 7), (10, 8), (10, 9), (10, 10)]
 # exe = [(10, 2)]
