69c9c8fe5ed955719d6c748f8f8dd3c93d28d308
sugu24 <shayasugu@icloud.com>
2024-01-15 12:25:42 +0900

    c

--- sudoku.cpp
+++ sudoku.cpp
@@ -332,7 +332,7 @@ int main(int argc, char *argv[]) {
 
     // ---------- result ---------- //
     std::fstream f;
-    file_name = "result_algoX.txt";
+    file_name = "result_algoX_new.txt";
     f.open(file_name, std::ios::app);
     f << BEAM << " " << MAX_PARENT << " " << 1 << " " << sudoku.getHints().size() << " " << sudoku.getConvergeCount14() << " " << END - START << " " << times << "hint ->";
     // 4 4 1 18 502188 1900 270 389 289 370 hint -> ( 0 1 0 ) ( 0 3 4 ) ( 0 7 8 ) ( 1 0 2 ) ( 1 4 6 ) ( 1 5 7 ) ( 2 0 5 ) ( 2 7 1 ) ( 3 1 4 ) ( 3 2 8 ) ( 3 3 1 ) ( 4 6 5 ) ( 5 2 7 ) ( 6 0 6 ) ( 6 4 5 ) ( 6 6 2 ) ( 7 7 4 ) ( 8 5 8 )
